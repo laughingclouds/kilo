528f3188d2d8da073036f7b810a3918d2147221c
laughingclouds <zenithtaiga@gmail.com>
2021-10-24 02:16:03 +0530

    structs and functions to Append Buffer rather than write "~" again and again.

--- abuf.h
+++ abuf.h
@@ -5,3 +5,9 @@ struct abuf {
     char *b;    // stands for buffer
     int len;
 };
+
+/* append `len` bytes of string s to ab->b (buffer) */
+extern void abAppend(struct abuf *ab, const char *s, int len);
+
+/* deallocates dynamic memory used by ab */
+extern void abFree(struct abuf *ab);
