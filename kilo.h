f5cbfd544c8048d971f28837aed10e84c37701e3
laughingclouds <zenithtaiga@gmail.com>
2021-10-24 02:04:26 +0530

    Added constant for package version. Updated struct editorConfig to include cursor coordinates.

--- kilo.h
+++ kilo.h
@@ -1,8 +1,10 @@
 #include <termios.h>
 
+#define KILO_VERSION "0.0.1"
 #define CTRL_KEY(k) ((k) & 0x1f)
 
 struct editorConfig {
+    int cx, cy; //cursor coordinates
     int screenrows;
     int screencols;
     struct termios orig_termios;
