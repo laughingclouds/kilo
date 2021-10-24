eb3d4dc97721771cf31bed97f979d998ceb6dfce
laughingclouds <zenithtaiga@gmail.com>
2021-10-24 14:23:24 +0530

    arrow key bindings

--- input.h
+++ input.h
@@ -2,5 +2,13 @@
   * I changed the original commands to that of vim */
 //extern void editorMoveCursor(char key);
 
+/**/
+enum editorKey {
+  ARROW_LEFT = 1000,
+  ARROW_RIGHT,
+  ARROW_UP,
+  ARROW_DOWN 
+};
+
 /* deal with custom key combinations */
 extern void editorProcessKeypress();
